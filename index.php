72468be65d0b713705ad7a7965832efa10a7156c
Desinta Yamasan Rusli <desintayamasanr@gmail.com>
2025-03-01 15:07:02 +0700

    add authcontroller,views
    penambahan authcontroller, dibagian views ditambah login and register beserta routes login register

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <?php
 session_start();
+require_once 'config/database.php'; #import database
 require_once 'routes/web.php';
 
 $url = isset($_GET['url']) ? $_GET['url'] : 'home';
--- routes/web.php
+++ routes/web.php
@@ -1,14 +1,28 @@
 <?php
-class Router {
-    public function route($url) {
-        switch($url) {
+class Router
+{
+    public function route($url)
+    {
+        switch ($url) {
             case 'home':
                 require_once 'app/controllers/HomeController.php';
                 $controller = new HomeController();
                 error_log('hello from home');
                 $controller->index();
                 break;
-            
+
+            case 'login':
+                require_once 'app/controllers/AuthController.php';
+                $controller = new AuthController();
+                $controller->login();
+                break;
+
+            case 'register':
+                require_once 'app/controllers/AuthController.php';
+                $controller = new AuthController();
+                $controller->register();
+                break;
+
             default:
                 header("HTTP/1.0 404 Not Found");
                 echo "Page not found";
